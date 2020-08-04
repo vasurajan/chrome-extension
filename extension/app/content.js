0cbf5cbe099f2f68110d9013ffe35645d969cf98
Geek <cleao.deuce@gmail.com>
2020-08-04 21:22:33 +0530

    Version 2.0.1
    Fixed the chrome extension

--- extension/app/content.js
+++ extension/app/content.js
@@ -1,2 +1,16 @@
-var name = window.prompt("What is your name?");
-alert("Welcome " + name)
+let button = document.getElementById('btn').addEventListener('click', getweather);
+
+
+
+function getweather() {
+    let city = document.getElementById('search').value;
+    let units = document.getElementById("units").value;
+
+    fetch('http://api.weatherstack.com/current?access_key=a236a39c8d90042cbd950759a75d0574&query=' + city + '&units=' + units)
+
+        .then(response => response.json())
+        .then(response => {
+            document.getElementById("image").src = response.current.weather_icons[0];
+            document.getElementById("output").innerHTML = "<h1>" + response.current.weather_descriptions[0] + "</h1> Temperature: " + response.current.temperature + "<br> Feels like" + response.current.feelslike + "<br> UV Index: " + response.current.uv_index + "<br> Humidity:" + response.current.humidity + "<br> Cloud Cover: " + response.current.cloudcover;
+        })
+}
